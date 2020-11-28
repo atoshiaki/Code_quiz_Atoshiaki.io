018fea19545b2916e845e30d093014f3120596cd
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-11-27 18:18:48 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,87 @@
-const startButton = document.getElementById("start-btn")
-const questionConttainerElement = document.getElementById("question-contianer")
+// sB = startButton
+const sB = document.getElementById("start-btn")
+// nB = nextButton
+const nB = document.getElementById("next-btn")
+// qCE = questionContainerElement
+const qCE = document.getElementById("questions-container")
+// qE = questionElement
+const qE = document.getElementById("questions")
+// aBE = answerButtonsElement
+const aBE = document.getElementById("answer-buttons")
+// sQ = shuffledQuestions, cQI = currentQuestionIndex
+let sQ, cQI
 
-startButton.addEventListener("click", startQuiz)
-//start quiz
-function startQuiz(){
-    console.log("start")
-    startButton.classList.add("hide")
-    questionConttainerElement.classList.remove("hide")
-}
-function startNextQuestion(){
+sB.addEventListener("click", startQuiz)
+nB.addEventListener("click", () => {
+  cQI++
+  setNextQuestion()
+})
 
+function startQuiz() {
+  sB.classList.add("hide")
+  sQ = questions.sort(() => Math.random() - .5)
+  cQI = 0
+  qCE.classList.remove("hide")
+  setNextQuestion()
 }
-function myAnswer(){
 
+function setNextQuestion() {
+  reset()
+  showQuestion(sQ[cQI])
 }
-const question = [
+
+function showQuestion(questions) {
+  qE.innerText = questions.questions
+  questions.answers.forEach(answer => {
+    const button = document.createElement("button")
+    button.innerText = answer.text
+    button.classList.add("btn")
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer)
+    aBE.appendChild(button)
+  })
+}
+
+function reset() {
+  clearStatusClass(document.body)
+  nB.classList.add("hide")
+  while (aBE.firstChild) {
+    aBE.removeChild(aBE.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(aBE.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (nextButtonsQ.length > cQI + 1) {
+    nB.classList.remove("hide")
+  } else {
+    sB.innerText = "Restart"
+    sB.classList.remove("hide")
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add("correct")
+  } else {
+    element.classList.add("wrong")
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct")
+  element.classList.remove("wrong")
+}
+
+const questions = [
         {
         title: "when calling Javascript, what is the correct tag you would use?",
         choices: ["<div>", "<section>", "<script>", "<header>"],
@@ -24,11 +91,6 @@ const question = [
         title: "The condition in an if/else statement is enclosed within ______.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
-    },
-    {
-        title: "What is the syntax for referencing an external script called 'abc.js'?",
-        choices: ["<script class='abc.js'>", "<script href='abc.js'>", "<script link='abc.js'>", "<script src='abc.js'>"],
-        answer: "<script src='abc.js'>"
     },
     {
         title: "Is JS case-sensitive?",
