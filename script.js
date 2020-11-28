8a1863521354e427a595cc6427f034fe860dfad5
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-11-27 17:38:24 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,83 +1,43 @@
-var start = document.getElementById("start");
-var submit = document.querySelector("button.submit")
-var timeLeft = (questions.length * 15 + 1);
-var timerEl = document.getElementById("timer");
-var submitScoreEl = document.querySelector("#submit-score");
-var userScoreEl = document.getElementById("user-score");
-var userNameInput;
-var questionHead = document.getElementById("questions");
-var answerChoices = document.getElementById("answers");
-var questionNumber = -1;
-var answer;
-function startTimer() {
-    document.getElementById("home").classList.add('d-none');
-    document.getElementById("quiz").classList.remove('d-none');
-    setTimer();
-    makeQuestions();
+const startButton = document.getElementById("start-btn")
+const questionConttainerElement = document.getElementById("question-contianer")
+
+startButton.addEventListener("click", startQuiz)
+//start quiz
+function startQuiz(){
+    console.log("start")
+    startButton.classList.add("hide")
+    questionConttainerElement.classList.remove("hide")
 }
-function setTimer() {
-    var countdown = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = "Time: " + timeLeft;
-        if (timeLeft === 0 || questionNumber === questions.length) {
-            clearInterval(countdown);
-            setTimeout(displayScore, 500);
-        }
-    }, 1000);
+function startNextQuestion(){
+
 }
-function makeQuestions() {
-    questionNumber++;
-    answer = questions[questionNumber].answer
-    questionHead.textContent = questions[questionNumber].title;
-    answerChoices.innerHTML = "";
-    var choices = questions[questionNumber].choices;
-    for (var q = 0; q < choices.length; q++) {
-        var nextChoice = document.createElement("button");
-        nextChoice.textContent = choices[q]
-        answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
-    }
+function myAnswer(){
+
 }
-function displayScore() {
-    document.getElementById("quiz").classList.add('d-none');
-    document.getElementById("submit-score").classList.remove('d-none');
-    userScoreEl.textContent = "Your final score is " + timeLeft + ".";
-}
-start.addEventListener("click", startTimer);
-submit.addEventListener("click", function (event) {
-    event.stopPropagation();
-    addScore();
-    window.location.href = './highscores.html'
-});
-function addScore () {
-    userNameInput = document.getElementById("userName").value
-    // create a new object with name and score keys
-var newScore = {
-        name: userNameInput,
-        score: timeLeft
-    };
-    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-    highScores.push(newScore)
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-}
-function hideFeedback(){
-    var pEl= document.getElementsByClassName("feedback")[0]
-    pEl.style.display='none'
-}
-function showFeedback(){
-    var pEl= document.getElementsByClassName("feedback")[0]
-    pEl.removeAttribute('style');
-}
-answerChoices.addEventListener("click", function (event) {
-    var pEl= document.getElementsByClassName("feedback")[0]
-    if (answer === event.target.textContent) {   
-        pEl.innerHTML = "Correct!";
-        setTimeout(hideFeedback,1000);
-        showFeedback();   
-    } else {
-        pEl.innerHTML = "Sorry, that's incorrect.";
-        setTimeout(hideFeedback,1000);
-        timeLeft = timeLeft - 10;
-        showFeedback();
-    }    
-    makeQuestions();
-});
+const question = [
+        {
+        title: "when calling Javascript, what is the correct tag you would use?",
+        choices: ["<div>", "<section>", "<script>", "<header>"],
+        answer: "<script>"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is the syntax for referencing an external script called 'abc.js'?",
+        choices: ["<script class='abc.js'>", "<script href='abc.js'>", "<script link='abc.js'>", "<script src='abc.js'>"],
+        answer: "<script src='abc.js'>"
+    },
+    {
+        title: "Is JS case-sensitive?",
+        choices: ["Yes", "No", "I have no clue", "Only when it feels like it."],
+        answer: "Yes"
+    },
+        {
+        title: "Which type of pop up box will allow a user to type a response?",
+        choices: ["input", "prompt", "alert", "confirm"],
+        answer: "prompt"
+    },
+]
